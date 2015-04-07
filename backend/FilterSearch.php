69f63fe5de4a58da7b60915b68f96bca1c8206d4
Jordan Watts <jdub9108@yahoo.com>
2015-04-07 15:55:22 -0700

    created filter search when button is clicked

--- backend/FilterSearch.php
+++ backend/FilterSearch.php
@@ -1,34 +1,82 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
+    
+    $search = 'men';
+    $by_age = 'Adult';
+    $by_sex = 'Male';
+    $by_type = 'Vest';
+    $by_group = '';
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+    /*$search = $_POST['searchterm'];
+    $by_age = $_POST['age_facet'];
+    $by_sex = $_POST['gender_facet'];
+    $by_type = $_POST['type_facet'];
+    $by_group = $_POST['group_facet'];*/
 
-    $by_age = $_GET['age'];
-    $by_sex = $_GET['gender'];
-    $by_fee = $_GET['fee'];
-    $by_group = $_GET['group'];
+    $query = "SELECT * FROM dbo.[Costume], dbo.[Dic_Costume_Type]";
+    $conditions = array('dbo.[Costume].Costume_Type_Key = dbo.[Dic_Costume_Type].Costume_Type_Key');
 
-    $query = "SELECT * FROM donar";
-    $conditions = array();
-
+    if($search !=""){
+        $conditions[] = "Costume_Name LIKE '%$search%'";
+    }
     if($by_age !="") {
-      $conditions[] = "Adult_or_Child='$by_age'";
+        $conditions[] = "Adult_or_Child='$by_age'";
     }
     if($by_sex !="") {
-      $conditions[] = "Costume_Gender='$by_sex'";
+        $conditions[] = "Costume_Gender='$by_sex'";
     }
-    if($by_fee !="") {
-      $conditions[] = "Rental_Fee='$by_fee'";
+    if($by_type !="") {
+        $conditions[] = "Costume_Type='$by_type'";
     }
     if($by_group !="") {
-      $conditions[] = "e_level='$by_group'";
+        $conditions[] = "Costume_Group='$by_group'";
     }
 
     $sql = $query;
     if (count($conditions) > 0) {
       $sql .= " WHERE " . implode(' AND ', $conditions);
     }
+    echo $sql;
+    
+    $server = 'cmt.cs87d7osvy2t.us-west-2.rds.amazonaws.com,1433';
+    $connectionInfo = array( "Database"=>"CMT", "UID"=>"admin", "PWD"=>"SJSUcmpe195");
+    $link = sqlsrv_connect($server, $connectionInfo);
+
+    //Checks connection
+    if (!$link) {
+        $output = "Problems with the database connection!"; 
+        $json = json_encode($output);
+        echo $json;
+    }   
+    
+    $stmt = sqlsrv_query($link,$sql);
+    if( $stmt === false ) {
+        die( print_r( sqlsrv_errors(), true));
+    }
+    
+    $rows = array();
+    $num_items_returned = 0;
+    $dir = '../lib/images/temp';
+    
+    while($result = sqlsrv_fetch_array( $stmt , SQLSRV_FETCH_ASSOC ))
+    {
+        // display results however we wan
+        $rows[] = $result;
+        $temp = $rows[$num_items_returned]['Costume_Image'];
+        file_put_contents($dir.'/test'.$num_items_returned.'.jpeg', $temp);
+        $rows[$num_items_returned]['Costume_Image'] = $dir.'/test'.$num_items_returned.'.jpeg';
+
+        $num_items_returned++;
+    }
+    
+    echo json_encode(array("searchterm"=> $search, "results"=>$rows, "numItems"=>$num_items_returned));
+    
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($link);
+                
 
 ?>
+</body>
+</html>
