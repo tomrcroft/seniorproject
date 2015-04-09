2c87c67672c5713632ea04e5967b7613be119f5a
Alexander Gilham <alexander.gilham@gmail.com>
2015-04-08 20:43:47 -0700

    Modified some CSS and added edit profile pages

---
+++ www/edit_profile.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Costume Inventory System | Dashboard</title>
+
+        <!-- Required header files -->
+        <script src="../lib/foundation/js/vendor/jquery.js" type="text/javascript"></script>
+        <script src="../lib/foundation/js/vendor/modernizr.js" type="text/javascript"></script>
+        <script src="../lib/foundation/js/foundation.min.js" type="text/javascript"></script>
+        <script src="../lib/js/search.js" type="text/javascript"></script>
+        <script src="../lib/js/logout.js" type="text/javascript"></script>
+
+        <link rel="stylesheet" href="../lib/foundation/css/foundation.css" type="text/css">
+        <link rel="stylesheet" href="../lib/foundation/css/normalize.css" type="text/css">
+        <link rel="stylesheet" href="../lib/css/index.css" type="text/css">
+        <link rel="stylesheet" href="../lib/css/main.css" type="text/css">
+        <link rel="stylesheet" href="../lib/css/search_page.css" type="text/css">
+
+    </head>
+
+    <body>
+        <nav class="top-bar" data-topbar role="navigation">
+            <ul class="title-area">
+                <!-- <img src="../lib/images/smallCMTlogo.jpg" alt="CMT" style="width:100px;height:110px">-->
+                <li class="name">
+                    <h1><a href="index.php">Costume Inventory System</a></h1>
+                </li>
+            </ul>
+
+            <section class="top-bar-section">
+                    <!-- Left Nav Section -->
+                <ul class="left">
+                    <li class="divider"></li>
+                    <li>
+                        <a href="#">Make a Pull Request</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="#1">Pull an entire set</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="order_status.php">Current Order Status</a>
+                    </li>
+                    <li class="divider"></li>
+                </ul>
+
+                <!-- Right Nav Section -->
+                <ul class="right">
+                    <li class="has-form">
+                        <div class="button" id="logout_button" value="Logout">Logout</div>
+                    </li>
+                </ul>
+            </section>
+        </nav>
+
+        <div class="registration large-3 large-centered columns">
+            <div class="login-box">
+                <div class="row">
+                    <div class="large-12 columns">
+                        <div class="form_title">Edit Profile</div>
+                        <div class="account_info">
+                            <div id="account_username">Username: </div>
+                            <div id="account_first_name">First Name: </div>
+                            <div id="account_last_name">Last Name: </div>
+                            <div id="account_company">Company: </div>
+                        </div>
+
+                        <form method="POST">
+                            <div class="row">
+                                <div class="large-12 columns">
+                                    <input type="text" name="firstname" id="update_firstname" placeholder="First Name" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="large-12 columns">
+                                    <input type="text" name="lastname" id="update_lastname" placeholder="Last Name" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="large-12 columns">
+                                    <input type="text" name="Company" id="update_company" placeholder="Company" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="large-12 columns">
+                                    <input type="text" name="email" id="update_email" placeholder="E-mail" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="large-12 columns">
+                                    <input type="password" name="password" id="update_password" placeholder="Password" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="large-12 large-centered columns">
+                                    <input class="button expand" id="update_button" value="Update Profile"/>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <script>
+        $(document).foundation();
+    </script>
+    </body>
+
+</html>
