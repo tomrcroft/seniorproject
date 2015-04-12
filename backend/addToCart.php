7973d3e92de1c0206aeadaa94d25d2886a3e81e0
Alexander Gilham <alexander.gilham@gmail.com>
2015-04-12 15:34:35 -0700

    Modified add to card and add item js

--- backend/addToCart.php
+++ backend/addToCart.php
@@ -7,7 +7,7 @@
 
 //check if available
 //add item id to session array
-    session_start();
+    // session_start();
     include '../backend/checkIfLoggedIn.php';
     $itemID = $_POST['itemID'];
     $_SESSION['shopping_cart'][] = $itemID;
