d8532880a61bf33abaec44bfaa04d33486d2dc81
Jordan Watts <jdub9108@yahoo.com>
2015-04-13 16:40:41 -0700

    made cancel pull request modified add to pull request

---
+++ backend/cancelPullRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    session_start();
+    $server = 'cmt.cs87d7osvy2t.us-west-2.rds.amazonaws.com,1433';
+    $connectionInfo = array( "Database"=>"CMT", "UID"=>"admin", "PWD"=>"SJSUcmpe195");
+    $link = sqlsrv_connect($server, $connectionInfo);
+    $formvars = array($_POST['pullId'],'Canceled',$_SESSION['login_user']);
+    //Checks connection
+    if (!$link) {
+        $output = "Problems with the database connection!"; 
+        $json = json_encode($output);
+        echo $json;
+    }        
+    else
+    {
+        $str = '{call dbo.Accept_Reject_Pull_Request(?,?,?)}';
+        $stmt = sqlsrv_query($link,$str,$formvars);//runs statement
+        if( $stmt === false ) {
+            die( print_r( sqlsrv_errors(), true));
+        }
+        sqlsrv_free_stmt($stmt);
+        sqlsrv_close($link);
+    }
+?>
