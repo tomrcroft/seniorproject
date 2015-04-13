0ead4070c20a419222ea7efec8d546220cc99b8b
Jordan Watts <jdub9108@yahoo.com>
2015-04-13 08:23:38 -0700

    added view cart

--- backend/viewCart.php
+++ backend/viewCart.php
@@ -4,10 +4,20 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+    
+    start_session();
+    if(!isset($_SESSION['shopping_cart']))
+        header ("Location: ../www/search_page.php");
+    $cart = $_SESSION['shopping_cart'];
     $server = 'cmt.cs87d7osvy2t.us-west-2.rds.amazonaws.com,1433';
     $connectionInfo = array( "Database"=>"CMT", "UID"=>"admin", "PWD"=>"SJSUcmpe195");
     $link = sqlsrv_connect($server, $connectionInfo);
 
+    //make query array from cart
+    foreach ($cart as $value) {
+        $conditions[] = "Costume_Key = $value";
+    }
+    
     //Checks connection
     if (!$link) {
         $output = "Problems with the database connection!"; 
@@ -17,7 +27,17 @@
     else
     {
         //need image,name,type(join),color,size,group,fee
-        echo '<hr>
+        $query = "SELECT Costume_Image,Costume_Name,Costume_Type,Costume_Color,Costume_Size,Costume_Group,Rental_Fee
+            FROM dbo.[Costume], dbo.[Dic_Costume_Type], dbo.[Dic_Costume_Color] WHERE
+            dbo.[Costume].Costume_Type_Key = dbo.[Dic_Costume_Type].Costume_Type_Key
+            AND dbo.[Costume].Costume_Color_Key = dbo.[Dic_Costume_Color].Costume_Color_Key";
+        //add str to query
+        $str = $query;
+        if (count($conditions) > 0) {
+            $str .= " AND ( " . implode(' OR ', $conditions) . " )";
+        }
+        echo $str;
+        /*echo '<hr>
         </div>
         <div class="row ">
           <div class="large-2 columns">
@@ -54,6 +74,6 @@
                 </div>
               </div>
             </div>
-          </div>';
+          </div>';*/
     }
 ?>
