df029babf419c6c261347783fede27ce155b211e
Jordan Watts <jdub9108@yahoo.com>
2015-04-14 14:48:57 -0700

    remove item from cart implemented

--- backend/removeFromCart.php
+++ backend/removeFromCart.php
@@ -3,4 +3,18 @@
 /*
  * Removes individual items from the shopping cart
  */
+
+    session_start();
+    $item = $_POST['itemId'];//item number to be removed
+    //$item = 42;
+    //$cart = array(40,17,42);
+    if(($key = array_search($item, $_SESSION['shopping_cart'])) !== false) {
+        unset($_SESSION['shopping_cart'][$key]);
+    }
+    /*if(($key = array_search($item, $cart)) !== false) {
+        unset($cart[$key]);
+    }
+    foreach ($cart as $value) {
+        echo $value . " ";
+    }*/
 ?>
