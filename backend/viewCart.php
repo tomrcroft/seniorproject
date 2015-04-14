7df5f285b5bf95c1745f60dbed518d090a0e4c7d
Jordan Watts <jdub9108@yahoo.com>
2015-04-14 12:40:45 -0700

    documentation additions

--- backend/viewCart.php
+++ backend/viewCart.php
@@ -1,8 +1,7 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Creates the view of all the items currently in your shopping cart
  */
     
     if(!isset($_SESSION['shopping_cart']))
