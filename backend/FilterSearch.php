da5b55156c47fd1345efd58e72f0262f4de4559b
Jordan Watts <jdub9108@yahoo.com>
2015-04-15 18:04:48 -0700

    filter updates

--- backend/FilterSearch.php
+++ backend/FilterSearch.php
@@ -8,39 +8,48 @@
 
     $search = trim($_POST['searchterm']);
     $by_age = trim($_POST['age_facet']);
+    $age_flag = 0;
     $by_sex = trim($_POST['gender_facet']);
+    $sex_flag = 0;
     $by_type = trim($_POST['type_facet']);
+    $type_flag = 0;
     $by_group = trim($_POST['group_facet']);
+    $group_flag = 0;
+    
+    $query = "SELECT * FROM dbo.[Costume], dbo.[Dic_Costume_Type]";//destroy
+    $conditions = array('dbo.[Costume].Costume_Type_Key = dbo.[Dic_Costume_Type].Costume_Type_Key');//destroy
 
-    $query = "SELECT * FROM dbo.[Costume], dbo.[Dic_Costume_Type]";
-    $conditions = array('dbo.[Costume].Costume_Type_Key = dbo.[Dic_Costume_Type].Costume_Type_Key');
-
-    if($search !=""){
-        $conditions[] = "Costume_Name LIKE '%$search%'";
-    }
+    if($search !=""){//destroy
+        $conditions[] = "Costume_Name LIKE '%$search%'";//destroy
+    }//destroy
     if($by_age !="") {
-        $conditions[] = "Adult_or_Child='$by_age'";
+        $conditions[] = "Adult_or_Child='$by_age'";//destroy
+        $age_flag = 1;
     }
     if($by_sex !="") {
-        $conditions[] = "Costume_Gender='$by_sex'";
+        $conditions[] = "Costume_Gender='$by_sex'";//destroy
+        $sex_flag = 1;
     }
     if($by_type !="") {
-        $conditions[] = "Costume_Type='$by_type'";
+        $conditions[] = "Costume_Type='$by_type'";//destroy
+        $type_flag = 1;
     }
     if($by_group !="") {
-        $conditions[] = "Costume_Group='$by_group'";
+        $conditions[] = "Costume_Group='$by_group'";//destroy
+        $group_flag = 1;
     }
 
     $sql = $query;
     if (count($conditions) > 1) {
-      $sql .= " WHERE " . implode(' AND ', $conditions);
+      $sql .= " WHERE " . implode(' AND ', $conditions);//destroy
     }
     // echo $sql;
     
     $server = 'cmt.cs87d7osvy2t.us-west-2.rds.amazonaws.com,1433';
     $connectionInfo = array( "Database"=>"CMT", "UID"=>"admin", "PWD"=>"SJSUcmpe195");
     $link = sqlsrv_connect($server, $connectionInfo);
-
+    //$sql = "{call dbo.FilterSearch(?,?,?,?,?,?,?,?,?)}";
+    //$params = array($search,$age_flag,$by_age,$sex_flag,$by_sex,$type_flag,$by_type,$group_flag,$by_group);
     //Checks connection
     if (!$link) {
         $output = "Problems with the database connection!"; 
@@ -48,7 +57,7 @@
         echo $json;
     }   
     
-    $stmt = sqlsrv_query($link,$sql);
+    $stmt = sqlsrv_query($link,$sql);//,$params
     if( $stmt === false ) {
         die( print_r( sqlsrv_errors(), true));
     }
@@ -59,7 +68,7 @@
     
     while($result = sqlsrv_fetch_array( $stmt , SQLSRV_FETCH_ASSOC ))
     {
-        // display results however we wan
+        // display results however we want photos format will be changed
         $rows[] = $result;
         $temp = $rows[$num_items_returned]['Costume_Image'];
         file_put_contents($dir.'/test'.$num_items_returned.'.jpeg', $temp);
