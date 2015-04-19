67f2eab76b44883d9f6d10d17a73cbba3492c4f9
Jordan Watts <jdub9108@yahoo.com>
2015-04-19 14:48:55 -0700

    changes to adding user addresses

--- backend/AddShippingAddress.php
+++ backend/AddShippingAddress.php
@@ -4,10 +4,12 @@
  * Adds the shipping address to the database
  */
     session_start();
+    if(!isset($_SESSION['user_id']))
+        include '../backend/getUserId.php';
     $server = 'cmt.cs87d7osvy2t.us-west-2.rds.amazonaws.com,1433';
     $connectionInfo = array( "Database"=>"CMT", "UID"=>"admin", "PWD"=>"SJSUcmpe195");
     $link = sqlsrv_connect($server, $connectionInfo);
-    $formvars = array($_SESSION['login_user'],$_POST['shipAddress'],$_POST['shipCity'],$_POST['shipState'],
+    $formvars = array($_SESSION['user_id'],$_POST['shipAddress'],$_POST['shipCity'],$_POST['shipState'],
         $_POST['shipAreaCode'],$_POST['shipCountry'],$_POST['shipAttn']);
     /*$formvars = array('jdub9108''123 cossa blvd','sac town','CA',95831,'US','MR. Watts');*/
     //Checks connection
@@ -22,10 +24,12 @@
             Shipping_State_Province,Shipping_Postal_Code,Shipping_Country,Shipping_Attn)
             VALUES (?,?,?,?,?,?,?)';
         $stmt = sqlsrv_query($link,$str,$formvars);//runs statement
-            if( $stmt === false ) {
-                die( print_r( sqlsrv_errors(), true));
-            }
-            sqlsrv_free_stmt($stmt);
-            sqlsrv_close($link);
+        if( $stmt === false ) {
+            die( print_r( sqlsrv_errors(), true));
+        }
+        sqlsrv_free_stmt($stmt);
+        sqlsrv_close($link);
+        $json = json_encode(array("location"=>"registration_billing.php", "error" => false));
+        exit($json);    
     }
 ?>
