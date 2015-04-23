6f490ee38799b4e9b772b67b9581273df5248371
abotella <abotella@users.noreply.github.com>
2015-04-22 19:27:16 -0700

    Update AdminViewPullRequest.php

--- backend/AdminViewPullRequest.php
+++ backend/AdminViewPullRequest.php
@@ -17,7 +17,7 @@ if (!$conn) {
     }        
     else
     {
-    	$formvars = array($_POST['pullrequestID']);
+    	$formvars = array($_POST['idnumber']);
 
     	//need picture, name, description, location
     	$query = "SELECT Costume_Image, Costume_Name, Costume_Description, Storage_Location 
@@ -28,9 +28,7 @@ if (!$conn) {
 
     	//run query
     	//$str = $query;
-
 		$stmt = sqlsrv_query($conn, $query, $formvars);
-
 		if( $stmt === false ) {
                 die( print_r( sqlsrv_errors(), true));
             }
