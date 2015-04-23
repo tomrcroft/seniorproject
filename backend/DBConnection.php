291648651832a6663e926a75199708ffeafba658
Jordan Watts <jdub9108@yahoo.com>
2015-04-23 14:02:28 -0700

    db connection update to fix pull request view

--- backend/DBConnection.php
+++ backend/DBConnection.php
@@ -7,7 +7,7 @@
 function connect()
 {
     $server = 'cmt.cs87d7osvy2t.us-west-2.rds.amazonaws.com,1433';
-    $connectionInfo = array( "Database"=>"CMT", "UID"=>"admin", "PWD"=>"SJSUcmpe195");
+    $connectionInfo = array( "Database"=>"CMT", "UID"=>"admin", "PWD"=>"SJSUcmpe195", "ReturnDatesAsStrings"=>"true");
     return sqlsrv_connect($server, $connectionInfo);
 }
 ?>
