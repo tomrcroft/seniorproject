6a1c6029edd584a96a17638b71d8e88e72cdcae9
Jordan Watts <jdub9108@yahoo.com>
2015-04-25 20:44:36 -0700

    invoice file updates

--- backend/ViewInvoices.php
+++ backend/ViewInvoices.php
@@ -17,7 +17,7 @@
     else
     {
         //run query
-        $str = "SELECT * FROM cmt..[Invoice_Hdr] WHERE Created_By = ? ORDER BY Status ASC";
+        $str = "SELECT * FROM cmt..[Invoice_Hdr] WHERE Username = ? ORDER BY Status,Created_Date ASC";
         $params = array($user);
         $stmt = sqlsrv_query($link,$str,$params);
         if( $stmt === false ) {
