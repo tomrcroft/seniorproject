17608b4d2fd570098ffcb095df082f2688f87f73
abotella <abotella@users.noreply.github.com>
2015-04-28 16:39:52 -0700

    Update view_master.php

--- backend/view_master.php
+++ backend/view_master.php
@@ -1,11 +1,5 @@
 <?php
 
-// check to make sure admin
- include '../backend/checkIfLoggedIn.php';
-    include '../backend/checkAdmin.php';
-    if(!checkIfAdmin($_SESSION['login_user']))
-        header ("Location: ../www/index.php");
-
 // connect to db first
 $serverName="cmt.cs87d7osvy2t.us-west-2.rds.amazonaws.com,1433";
 $database="CMT";
@@ -16,7 +10,7 @@ $password="SJSUcmpe195";
 $connectionInfo = array( "UID"=>$username, "PWD"=>$password, "Database"=>$database);
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
-$formvars = array($_POST['company']);
+$formvars = array($_POST['companySearch']);
 //all pull requests
  $num_items_returned = 0;
 
@@ -36,19 +30,19 @@ while($result = sqlsrv_fetch_array( $query, SQLSRV_FETCH_ASSOC ))
                         </div>
                        ';
 
-
 //$rows_pulls[] = $result;
 //$num_items_returned++;
 }
+sqlsrv_free_stmt($result);
 
-$query = sqlsrv_query( $conn, "SELECT * FROM cmt..[Invoice_Hdr], cmt..[User]
+$query1 = sqlsrv_query( $conn, "SELECT * FROM cmt..[Invoice_Hdr], cmt..[User]
                                WHERE cmt.[User].Company = ?
                                AND cmt.[User].Username = cmt..[Invoice_Hdr].Username", $formvars);
 	 	echo '	</div>
                     <div class="large-4 large-offset-2 columns left" id="records_results">
                         <h5>COMPANYNAME View Invoice Records for USER (2 Results)</h5> ';
 
-while($results = sqlsrv_fetch_array( $query, SQLSRV_FETCH_ASSOC ));
+while($results = sqlsrv_fetch_array( $query1, SQLSRV_FETCH_ASSOC ));
 {
 
 	$datemodified = date_format($results['Last_Modified_Date'], 'm-d-Y');
@@ -65,6 +59,7 @@ while($results = sqlsrv_fetch_array( $query, SQLSRV_FETCH_ASSOC ));
 }
 
 //echo json_encode(array("searchterm"=> $find, "results1"=>$rows_pulls, "results2" =>$rows_invoice, "numItems"=>$num_items_returned));
-
+sqlsrv_free_stmt($result);
+        sqlsrv_close($conn);
 
 ?>
