f1c7a3a591ea54533c2f81b4790eff53baacd279
abotella <abotella@users.noreply.github.com>
2015-04-28 16:53:09 -0700

    Update view_master.php

--- backend/view_master.php
+++ backend/view_master.php
@@ -14,18 +14,19 @@ $formvars = array($_POST['companySearch']);
 //all pull requests
  $num_items_returned = 0;
 
-$query = sqlsrv_query( $conn, "SELECT * FROM cmt..[Pull_Request_Hdr], cmt..[User]
+$query = "SELECT * FROM cmt..[Pull_Request_Hdr], cmt..[User]
 							   WHERE cmt..[User].Company = ?
 							   AND cmt..[User].Username = cmt..[Pull_Request_Hdr].Created_By
-							   ORDER BY Status ASC", $formvars);
+							   ORDER BY Status ASC";
+$stmt = sqlsrv_query($conn, $query, $formvars);
 //display results
-while($result = sqlsrv_fetch_array( $query, SQLSRV_FETCH_ASSOC ))
+while($result = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC ))
 {	
 	 $datemodified = date_format($result['Last_Modified_Date'], 'm-d-Y');
 
 
-                    echo '    <div class="admin_pull_results panel" id='. $result[Pull_Request_ID] .'>
-                            <h5>' .$result[Production]. '</h5> 
+                    echo '    <div class="admin_pull_results panel" id='. $result['Pull_Request_ID'] .'>
+                            <h5>' .$result['Production']. '</h5> 
                             DATE MODIFIED: '. $datemodified .'
                         </div>
                        ';
@@ -33,22 +34,22 @@ while($result = sqlsrv_fetch_array( $query, SQLSRV_FETCH_ASSOC ))
 //$rows_pulls[] = $result;
 //$num_items_returned++;
 }
-sqlsrv_free_stmt($result);
+sqlsrv_free_stmt($stmt);
 
-$query1 = sqlsrv_query( $conn, "SELECT * FROM cmt..[Invoice_Hdr], cmt..[User]
+$query1 = "SELECT * FROM cmt..[Invoice_Hdr], cmt..[User]
                                WHERE cmt.[User].Company = ?
-                               AND cmt.[User].Username = cmt..[Invoice_Hdr].Username", $formvars);
+                               AND cmt.[User].Username = cmt..[Invoice_Hdr].Username";
 	 	echo '	</div>
                     <div class="large-4 large-offset-2 columns left" id="records_results">
                         <h5>COMPANYNAME View Invoice Records for USER (2 Results)</h5> ';
-
+$stmt = sqlsrv_query($conn, $query1, $formvars);
 while($results = sqlsrv_fetch_array( $query1, SQLSRV_FETCH_ASSOC ));
 {
 
 	$datemodified = date_format($results['Last_Modified_Date'], 'm-d-Y');
 
-                    echo '    <div class="admin_invoice_results panel" id='. $results[Pull_Request_ID] .'>
-                            <h5> '.$results[Billing_Name].'</h5> 
+                    echo '    <div class="admin_invoice_results panel" id='. $results['Pull_Request_ID'] .'>
+                            <h5> '.$results['Billing_Name'].'</h5> 
                             DATE MODIFIED: '. $datemodified .'
                         </div>
                         ';
@@ -59,7 +60,7 @@ while($results = sqlsrv_fetch_array( $query1, SQLSRV_FETCH_ASSOC ));
 }
 
 //echo json_encode(array("searchterm"=> $find, "results1"=>$rows_pulls, "results2" =>$rows_invoice, "numItems"=>$num_items_returned));
-sqlsrv_free_stmt($result);
+sqlsrv_free_stmt($stmt);
         sqlsrv_close($conn);
 
 ?>
