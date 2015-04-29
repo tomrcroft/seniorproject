391a08b7f8168f1209cc994f1f7b4d9141c2aa0c
abotella <abotella@users.noreply.github.com>
2015-04-28 17:34:09 -0700

    Update view_master.php

--- backend/view_master.php
+++ backend/view_master.php
@@ -19,6 +19,7 @@ $query = "SELECT * FROM cmt..[Pull_Request_Hdr], cmt..[User]
 							   AND cmt..[User].Username = cmt..[Pull_Request_Hdr].Created_By
 							   ORDER BY Status ASC";
 $stmt = sqlsrv_query($conn, $query, $formvars);
+
 //display results
 while($result = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC ))
 {	
@@ -46,6 +47,7 @@ $query1 = "SELECT * FROM cmt..[Invoice_Hdr], cmt..[User]
 	 	echo '	</div>
                     <div class="large-4 large-offset-2 columns left" id="records_results">
                         <h5>COMPANYNAME View Invoice Records for USER (2 Results)</h5> ';
+
 $stmt1 = sqlsrv_query($conn, $query1, $formvars);
 while($results = sqlsrv_fetch_array( $stmt1, SQLSRV_FETCH_ASSOC ));
 {
@@ -53,7 +55,8 @@ while($results = sqlsrv_fetch_array( $stmt1, SQLSRV_FETCH_ASSOC ));
         die( print_r( sqlsrv_errors(), true));
     }
 	
-        $datemodified1 = date_format($results['Last_Modified_Date'], 'm-d-Y');
+	$date = date_create($results['Last_Modified_Date']);
+        $datemodified1 = date_format($date, 'm-d-Y');
 
                     echo '    <div class="admin_invoice_results panel" id='. $results['Pull_Request_ID'] .'>
                             <h5> '.$results['Billing_Name'].'</h5> 
@@ -67,7 +70,6 @@ while($results = sqlsrv_fetch_array( $stmt1, SQLSRV_FETCH_ASSOC ));
 //echo json_encode(array("searchterm"=> $find, "results1"=>$rows_pulls, "results2" =>$rows_invoice, "numItems"=>$num_items_returned));
 sqlsrv_free_stmt($stmt1);
 
-// //echo json_encode(array("searchterm"=> $find, "results1"=>$rows_pulls, "results2" =>$rows_invoice, "numItems"=>$num_items_returned));
 // sqlsrv_free_stmt($stmt);
 
         sqlsrv_close($conn);
