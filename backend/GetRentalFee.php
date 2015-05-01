7f971e3fb427f4a48ffe58d82cf99a0bbe95ddb9
Jordan Watts <jdub9108@yahoo.com>
2015-05-01 12:01:49 -0700

    even more db connection changes

--- backend/GetRentalFee.php
+++ backend/GetRentalFee.php
@@ -6,9 +6,8 @@
  */
 
 //db connection
-$server = 'cmt.cs87d7osvy2t.us-west-2.rds.amazonaws.com,1433';
-$connectionInfo = array( "Database"=>"CMT", "UID"=>"admin", "PWD"=>"SJSUcmpe195");
-$link = sqlsrv_connect($server, $connectionInfo);
+include '../backend/DBConnection.php';
+$link = connect();
 
 //Checks connection
 if (!$link) {
